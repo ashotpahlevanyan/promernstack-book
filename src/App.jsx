d5f1d9d06c8edab1ecd60514fc853d95923105e0
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-03-11 03:33:46 +0400

    tried with br as separator for the array display

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-var contentNode = document.getElementById('contents');
+const contentNode = document.getElementById('contents');
 
 const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+const br = <br />;
+const message = continents.map(c => `Hello ${c}!`).join(`${br}`);
 
-const component = <p>{message}</p>
+const component = <p>{message}</p>;
 
 ReactDOM.render(component, contentNode);
--- static/App.js
+++ static/App.js
@@ -3,9 +3,10 @@
 var contentNode = document.getElementById('contents');
 
 var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var br = React.createElement('br', null);
 var message = continents.map(function (c) {
   return 'Hello ' + c + '!';
-}).join(' ');
+}).join('' + br);
 
 var component = React.createElement(
   'p',
