57bbeb412e01b386e295f27186e97bee89b123e2
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-03-18 15:40:35 +0400

    server doesn't work, compilation to vendor and app doesn't work

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import 'babel-polyfill';
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import Issue from './issue';
+const MongoClient = require('mongodb').MongoClient;
+import Issue from './issue.js';
 
 SourceMapSupport.install();
 
--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -85,7 +85,6 @@ const IssueRow = props => (
     <td>
       <Link to={`/issues/${props.issue._id}`}>{props.issue._id.substr(-4)}</Link>
     </td>
-    <td>{props.issue._id}</td>
     <td>{props.issue.status}</td>
     <td>{props.issue.owner}</td>
     <td>{props.issue.created.toDateString()}</td>
