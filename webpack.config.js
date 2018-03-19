5ec5392f7690a0f20239a06cebc61331376c56af
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-03-19 20:06:07 +0400

    webpack dev server, no hmr

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,10 @@ module.exports = {
 		contentBase: 'static',
 		proxy: {
 			'/api/*': {
-				target: 'http://localhost:3000/'
+				target: 'http://localhost:3000'
 			}
-		}
+		},
+		hot: true
 	},
 	output: {
 		path: path.resolve(__dirname, "static"),
